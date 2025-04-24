ba727075fca49a945c5322f0483e2f5c1d453d44
CSU22-AINF <CSU22@academico.unab.ad>
2025-04-24 11:59:52 -0500

    Se configuro el login

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,15 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+
 
 Route::prefix('/productos')->controller(ProductController::class)->group(function () {
     Route::get('/', 'index');
     Route::get('/create', 'create');
     Route::get('/{name}', 'show');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'welcome']);
